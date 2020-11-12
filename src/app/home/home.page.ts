1ad2fedc0f176cdc70b935b941e87f635c962385
Chadd Williams <chadd@ribitt.io>
2020-11-12 12:34:32 -0500

    added camera and filesystem functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CameraPhoto, CameraResultType, CameraSource, Capacitor, FilesystemDirectory, FilesystemEncoding, Plugins } from '@capacitor/core';
+
+const { Filesystem } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  public async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 75,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera,
+      height: 250,
+    });
+
+    console.log(image);
+    this.fileWrite(image.base64String);
+  }
+
+  public async fileWrite(f: any) {
+    try {
+      const result = await Filesystem.writeFile({
+        path: 'test/test.png',
+        data: f,
+        directory: FilesystemDirectory.Documents,
+        encoding: FilesystemEncoding.UTF8,
+      });
+      console.log('Wrote file', result);
+      // this.fileRead(f);
+    } catch (e) {
+      console.error('Unable to write file', e);
+    }
+  }
 
 }
